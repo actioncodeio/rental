a95bfd0b4d2cb46cc1f35ca70b82766a8bdea964
bragamat <mateuscbraga@gmail.com>
2022-03-31 14:45:55 -0300

    start to add client test

---
+++ __tests__/client/select.spec.ts
@@ -0,0 +1,12 @@
+describe('Client', () => {
+  describe('select', () => {
+    it.todo('passes tablename and what to select and query')
+    it.todo('selects * from table name if no option is given')
+
+    describe('Concatenating with other clauses', () => {
+      it.todo('concatenates with WHERE')
+      it.todo('concatenates with COUNT')
+      it.todo('concatenates with GroupBy')
+    })
+  })
+})
